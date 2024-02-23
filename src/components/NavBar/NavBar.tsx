6aa4e6d486d0a21968e0d436abe6e84336f97dbc
chunxtan <chunxiaotan99@gmail.com>
2024-02-23 14:07:50 +0800

    set up save customise dataset form

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -13,7 +13,28 @@ export const NavBar = observer(({ userStore }: NavBarProps): React.JSX.Element =
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        
+
+                {/* NavBar Start */}
+                <div>
+                    <Link to="/" className="btn btn-ghost text-xl">WALKING WIZARD</Link>
+                </div>
+
+                {/* NavBar Center */}
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <Link to="/datasets" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
+                                Datasets
+                            </Link>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Scenarios
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
                 {/* NavBar End */}
                 {
                     userStore.user
@@ -47,22 +68,6 @@ export const NavBar = observer(({ userStore }: NavBarProps): React.JSX.Element =
                         <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><Link to="/login-signup">Login/Sign up</Link></button>
                     </div>
                 }
-
-            
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                        <Link to="/datasets" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Datasets</Link>
-                        </li>
-                        <li>
-                        <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Scenarios</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div>
-                    <Link to="/" className="btn btn-ghost text-xl">WALKING WIZARD</Link>
-                </div>
 
             </div>
         </nav>
